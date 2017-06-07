c54cc92a3c2bc991d838731d1c2cb68abfc5cc78
hernanda0684 <hernanda0684@student.faytechcc.edu>
2017-06-06 23:03:04 -0400

    Created my first Sales Prediction

---
+++ M2T1_Sales Prediction-Hernandez-Diaz.py
@@ -0,0 +1,8 @@
+# Creating a Sales Prediction
+# Date: 06/06/2017
+# CTI-110 M2T1 - Sales Prediction
+# Hernandez-Diaz, Andrae
+#
+total_sales = float(input('Enter the projected sales: '))
+profit = total_sales * 0.23
+print('The profit is $', profit)

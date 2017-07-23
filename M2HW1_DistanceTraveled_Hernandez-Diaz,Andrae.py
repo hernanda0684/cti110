162f089e4619aed2bfbd57264c7de25113b21b54
hernanda0684 <hernanda0684@student.faytechcc.edu>
2017-07-22 23:47:55 -0400

    M2HW1_DistanceTraveled_Hernandez-Diaz,Andrae /

---
+++ M2HW1_DistanceTraveled_Hernandez-Diaz,Andrae.py
@@ -0,0 +1,11 @@
+# Calculating Distance Traveled
+# Date: 06/07/2017
+# CTI-110 M2HW1 - Distance Traveled
+# Hernandez-Diaz, Andrae
+#
+distance = float(input('Enter distance: '))
+miles_hours6 = 70 * 6
+miles_hours10 = 70 * 10
+miles_hours15 = 70 * 15
+total = miles_hours6 / distance, miles_hours10 / distance, miles_hours15 / distance
+print('Distance Traveled is', total)
